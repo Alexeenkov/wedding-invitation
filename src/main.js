72d281b59293d232b7e8720d0d077dc0c3de9924
Ilya Alekseenkov <alekseenkov.i@promail366.com>
2025-04-07 12:03:31 +0300

    fix vue-router

--- src/main.js
+++ src/main.js
@@ -2,23 +2,23 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/view/HomeView.vue';
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const router = createRouter({
     routes: [{
-        path: '/',
-        component: HomeView
+        path: '/wedding-invitation/',
+        component: HomeView,
     }],
-    history: createWebHistory()
+    history: createWebHistory(),
 })
+
+createApp(App)
+    .use(router)
+    .use(AOS)
+    .mount('#app');
 
 router.beforeEach((to, from, next) => {
     AOS.init({once: true});
     next();
 });
-
-createApp(App)
-    .use(router)
-    .use(AOS)
-    .mount('#app')

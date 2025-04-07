33d2b6b6c0865169b325fd4c327649ce11099df2
Ilya Alekseenkov <alekseenkov.i@promail366.com>
2025-04-07 11:14:42 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './wedding-invitation',
+  base: '/wedding-invitation/',
   css: {
     preprocessorOptions: {
       scss: {

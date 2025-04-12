e83a81eb00f7eaaac77ec4fd975dae0247c6c53a
Ilya Alekseenkov <alekseenkov.i@promail366.com>
2025-04-12 15:17:13 +0300

    fix colors

---
+++ src/components/icons/IcoCopy.vue
@@ -0,0 +1,1 @@
+<template>  <div>    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">      <path fill-rule="evenodd" d="M19.5 16.5v-12l-.75-.75H9l-.75.75v3h-3l-.75.75v12l.75.75H15l.75-.75v-3h3l.75-.75Zm-3.75-.75v-7.5L15 7.5H9.75V5.25H18v10.5h-2.25ZM6 9h8.25v10.5H6V9Z" clip-rule="evenodd"/>    </svg>  </div></template>
